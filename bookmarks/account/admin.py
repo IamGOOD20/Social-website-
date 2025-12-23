63e8cd29dd337b222d77fad6027855fb055ce0af
Yevhen <e.rukshenas.tech@gmail.com>
2025-12-22 22:24:46 -0500

    Extended user model

--- bookmarks/account/admin.py
+++ bookmarks/account/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-
+from .models import Profile
 # Register your models here.
 # Admin
 # admin123
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'date_of_birth', 'photo']
+    raw_id_fields = ['user']
